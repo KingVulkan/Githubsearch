964c4e0d059f7aaefb18c39432fb781811e4c5fa
KingVulkan <manowfelow@gmail.com>
2019-01-21 19:31:06 +0300

    adds functionality in  profile component

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid = 'c5a1730ce870d69e94e7';
+  private clientsecret = 'aeab2f70b3eabd646a340e98e26644e0b9618a74';
+
+  constructor(private http: Http) {
+    console.log('service is now ready!');
+    this.username = 'KingVulkan';
+  }
+
+  getProfileInfo() {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+      .map(res => res.json());
+  }
+
+  getProfileRepos() {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+      .map(res => res.json());
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
+
 }
+
